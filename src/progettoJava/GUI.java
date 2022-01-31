65e9ca23fcb307df850058bf3c9b3c902e21a307
dosquill <domenicods.dsq@gmail.com>
2022-01-31 16:50:25 +0100

    first commit progetto corretto

---
+++ src/progettoJava/GUI.java
@@ -0,0 +1,415 @@
+/*
+		TODO
+	-Problema query6
+	-Messaggio di errore nella modifica di una tessera
+*/
+
+package progettoJava;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JRadioButton;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.Font;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class GUI {
+
+	private JFrame frame;
+	
+    private String DB_URL="jdbc:mysql://localhost:3306/progettoesame";
+    private String USER="root";
+    private String PASS="1234"; 
+	
+	private String query1 = "SELECT * \r\n"
+	    		+ "FROM Tennista as t\r\n"
+	    		+ "WHERE t.Eta >= 18 AND (t.Sesso = \"Maschio\" OR t.Sesso = \"Femmina\")\r\n"
+	    		+ "ORDER BY t.Eta;";
+	    
+	private String query2 = "SELECT m.MatricolaConi, m.Nome, m.Cognome\r\n"
+			+ "FROM Maestro as m JOIN Insegnamento as i ON m.MatricolaConi = i.MatricolaConi\r\n"
+			+ "WHERE i.Codice = 1001\r\n"
+			+ ";";
+	    
+	private String query3 = "SELECT Sum(t.IncassoTotale)\r\n"
+	    		+ "FROM Torneo as t\r\n"
+	    		+ ";";
+	    
+	private String query4 = "SELECT AVG(t.Eta)\r\n"
+	    		+ "FROM Tennista as t\r\n"
+	    		+ "GROUP BY t.Sesso\r\n"
+	    		+ ";";
+	    
+	private String query5 = "SELECT t.Nome, t.Citta, t.IncassoTotale\r\n"
+	    		+ "FROM Torneo as t\r\n"
+	    		+ "GROUP BY t.Nome, t.Citta\r\n"
+	    		+ "HAVING t.IncassoTotale > 50\r\n"
+	    		+ ";";
+	    
+	private String query6 = "SELECT *\r\n"
+	    		+ "FROM Torneo as t\r\n"
+	    		+ "GROUP BY t.Nome, t.Citta\r\n"
+	    		+ "HAVING t.IncassoTotale >= ALL (\r\n"
+	    		+ "	SELECT t1.IncassoTotale\r\n"
+	    		+ "    FROM Torneo as t1\r\n"
+	    		+ "    GROUP BY t1.Nome, t1.Citta\r\n"
+	    		+ "    )";
+	    
+	private String query7 = "SELECT t.Nome, t.Citta\r\n"
+	    		+ "FROM Torneo as t\r\n"
+	    		+ "WHERE t.giorno NOT IN (\r\n"
+	    		+ "	SELECT t1.giorno\r\n"
+	    		+ "    FROM Torneo as t1\r\n"
+	    		+ "    WHERE t1.giorno >= \"2022-03-01\" AND t1.giorno <= \"2022-03-31\"\r\n"
+	    		+ "    )\r\n"
+	    		+ ";";
+	    
+	private String query8 = "SELECT Tennista.Tessera, Tennista.Nome, Tennista.Cognome\r\n"
+	    		+ "FROM Tennista\r\n"
+	    		+ "WHERE NOT EXISTS (\r\n"
+	    		+ "	SELECT *\r\n"
+	    		+ "    FROM Torneo\r\n"
+	    		+ "    WHERE Torneo.Citta = \"Pagani\" AND NOT EXISTS (\r\n"
+	    		+ "			SELECT *\r\n"
+	    		+ "            FROM Partecipare\r\n"
+	    		+ "            WHERE Partecipare.Tessera = Tennista.Tessera AND Torneo.Citta = Partecipare.Citta AND Torneo.Nome = Partecipare.Nome\r\n"
+	    		+ "		)\r\n"
+	    		+ "    )\r\n"
+	    		+ ";\r\n"
+	    		+ "";
+	private JTextField textFieldTessera;
+	private JTextField textFieldEta;
+	    
+	
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 574, 385);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Query 1", "Query 2", "Query 3", "Query 4", "Query 5", "Query 6", "Query 7", "Query 8"}));
+		comboBox.setBounds(10, 11, 100, 30);
+		frame.getContentPane().add(comboBox);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(10, 146, 538, 189);
+		frame.getContentPane().add(textArea);
+		
+		JButton submitButton = new JButton("Submit");
+		submitButton.setBounds(10, 58, 89, 23);
+		frame.getContentPane().add(submitButton);
+		
+		JTextArea textAreaTop = new JTextArea();
+		textAreaTop.setBounds(10, 116, 538, 30);
+		frame.getContentPane().add(textAreaTop);
+		
+		JButton stampaButton = new JButton("Stampa");
+		stampaButton.setBounds(260, 58, 81, 23);
+		frame.getContentPane().add(stampaButton);
+		
+		JButton editButton = new JButton("Modifica");
+		editButton.setBounds(351, 58, 85, 23);
+		frame.getContentPane().add(editButton);
+		
+		JButton removeButton = new JButton("Rimozione");
+		removeButton.setBounds(449, 58, 99, 23);
+		frame.getContentPane().add(removeButton);
+		
+		textFieldTessera = new JTextField();
+		textFieldTessera.setBounds(344, 21, 72, 20);
+		frame.getContentPane().add(textFieldTessera);
+		textFieldTessera.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Tessera: ");
+		lblNewLabel.setBounds(282, 27, 62, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Eta: ");
+		lblNewLabel_1.setBounds(452, 27, 34, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		textFieldEta = new JTextField();
+		textFieldEta.setBounds(486, 21, 62, 20);
+		frame.getContentPane().add(textFieldEta);
+		textFieldEta.setColumns(10);
+		
+		JButton addButton = new JButton("Aggiungi");
+		addButton.setBounds(159, 58, 90, 23);
+		frame.getContentPane().add(addButton);
+		
+		JTextArea descriptionArea = new JTextArea();
+		descriptionArea.setBounds(10, 95, 538, 22);
+		frame.getContentPane().add(descriptionArea);
+
+		
+		
+		
+		// CONNESSIONE
+		submitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				textArea.setText("");
+				textAreaTop.setText("");
+				String queryExecuted ="";
+				String descrizione ="";
+				
+				switch (comboBox.getSelectedItem().toString()) {
+				case "Query 1" ->{
+					queryExecuted = query1;
+					descrizione = "Stampare tutti i tennisti con età maggiore/uguale di 18";
+				}
+
+				case "Query 2" ->{					
+					queryExecuted = query2;
+					descrizione = "Stampare i maestri che insegnano alla lezione con codice 1001\r\n"
+							+ "";
+				}
+
+				case "Query 3" ->{
+					queryExecuted = query3;
+					descrizione = "Stampare la somma di tutti gli incassi totali dei tornei\r\n"
+							+ "";
+				}
+
+				case "Query 4" ->{
+					queryExecuted = query3;
+					descrizione = "Stampare la media di età tra i tennisti maschi e tennisti femmine\r\n"
+							+ "";
+				}
+				
+				case "Query 5" ->{
+					queryExecuted = query3;
+					descrizione = "Stampare i tornei il cui Incasso Totale è maggiore di 50\r\n"
+							+ "";
+				}
+				
+				case "Query 6" ->{
+					queryExecuted = query3;
+					descrizione = "Stampare il torneo che possiede l'incasso totale più alto\r\n"
+							+ "";
+				}
+				
+				case "Query 7" ->{
+					queryExecuted = query3;
+					descrizione = "Selezionare il nome e la città dei tornei che non si svolgono a Marzo\r\n"
+							+ "";
+				}
+				
+				case "Query 8" ->{
+					queryExecuted = query3;
+					descrizione = "Stampare tessera, nome e cognome di tutti i tennisti che partecipano al torneo nella città di Pagani*\r\n"
+							+ "";
+				}
+				
+				default ->
+				throw new IllegalArgumentException("Unexpected value: " + comboBox);
+				}
+				
+		
+				try {
+					Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);	
+					PreparedStatement statement = connection.prepareStatement(queryExecuted);
+					ResultSet result = statement.executeQuery(queryExecuted);
+					ResultSetMetaData rsmd = result.getMetaData();
+					
+					int k = 1;
+					
+					while (result.next()) {						
+						for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+							if (i > 1)
+								System.out.println(", ");
+							
+							if (k == i) {
+								textAreaTop.append(rsmd.getColumnName(k) + "\t");								
+								k++;
+							}
+							
+							textArea.append(result.getString(i) + "\t");
+						}
+						
+						textArea.append("\n");
+					}
+					
+					descriptionArea.setText("Descrizione: " + descrizione);
+					
+					statement.close();
+					connection.close();
+				} catch (SQLException ex) {
+					System.out.println("Cannot connect");
+					ex.printStackTrace();
+				}
+				
+			}
+		});
+	    
+		
+		// STAMPA TUTTI I TENNISTI
+		stampaButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String queryStampa = "SELECT * FROM Tennista;";
+				
+				textArea.setText("");
+				textAreaTop.setText("");
+				descriptionArea.setText("Stampa di tutti i tennisti");
+				
+				try {
+					Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);	
+					PreparedStatement statement = connection.prepareStatement(queryStampa);
+					ResultSet result = statement.executeQuery(queryStampa);
+					ResultSetMetaData rsmd = result.getMetaData();
+					
+					int k = 1;
+					
+					while (result.next()) {						
+						for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+							if (i > 1)
+								System.out.println(", ");
+							
+							if (k == i) {
+								textAreaTop.append(rsmd.getColumnName(k) + "\t");								
+								k++;
+							}
+							
+							textArea.append(result.getString(i) + "\t");
+						}
+						
+						textArea.append("\n");
+					}
+					
+					
+					statement.close();
+					connection.close();
+				} catch (SQLException ex) {
+					System.out.println("Cannot connect");
+					ex.printStackTrace();
+				}
+			}
+		});
+		
+		
+		
+		// MODIFICA
+		editButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int tessera = Integer.parseInt(textFieldTessera.getText());
+				int eta  = Integer.parseInt(textFieldEta.getText());
+				
+				String sql = "UPDATE Tennista\r\n"
+						+ "SET Tennista.Eta = " + eta + "\r\n" 
+						+ "WHERE Tennista.Tessera = " + tessera + ";";
+				
+				try {
+					Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);	
+					PreparedStatement statement = connection.prepareStatement(sql);
+					statement.executeUpdate();
+										
+					System.out.println("modifica avvenuta");
+					
+					statement.close();
+					connection.close();
+					stampaButton.doClick();
+				} catch (SQLException ex) {
+					System.out.println("Cannot connect");
+					ex.printStackTrace();
+				}
+				
+			}
+		});
+
+		
+		
+		
+		// RIMOZIONE
+		removeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+				try {
+					int tessera = Integer.parseInt(textFieldTessera.getText());
+					String update1 = "SET FOREIGN_KEY_CHECKS=0;";
+					String sql = "DELETE FROM Tennista WHERE Tennista.Tessera = " + tessera + ";";
+					String update2 = "SET FOREIGN_KEY_CHECKS=1;";
+
+					Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);	
+					PreparedStatement statementUpd1 = connection.prepareStatement(update1);
+					PreparedStatement statement = connection.prepareStatement(sql);
+					PreparedStatement statementUpd2 = connection.prepareStatement(update2);
+					
+					statementUpd1.executeUpdate();					
+					statement.executeUpdate();
+					statementUpd2.executeUpdate();
+					
+					statement.close();
+					connection.close();
+					stampaButton.doClick();
+				} catch (SQLException ex) {
+					System.out.println("Cannot connect");
+					ex.printStackTrace();
+				}
+			
+			}
+		});
+	
+	
+		// AGGIUNTA TENNISTA
+		addButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addFrame frameAggiunta = new addFrame();
+				frameAggiunta.setVisible(true);
+				
+			}
+		});
+	
+	
+	
+	
+	}
+}
---
+++ src/progettoJava/addFrame.java
@@ -0,0 +1,141 @@
+package progettoJava;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+
+public class addFrame extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField tesseraField;
+	private JTextField nomeField;
+	private JTextField cognomeField;
+	private JTextField sessoField;
+    private String DB_URL="jdbc:mysql://localhost:3306/progettoesame";
+    private String USER="root";
+    private String PASS="1234"; 
+	
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					addFrame frame = new addFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public addFrame() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 403, 177);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Tessera:");
+		lblNewLabel.setBounds(10, 32, 59, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setBounds(10, 63, 46, 14);
+		contentPane.add(lblNome);
+		
+		JLabel lblCognome = new JLabel("Cognome:");
+		lblCognome.setBounds(190, 63, 63, 14);
+		contentPane.add(lblCognome);
+		
+		JLabel lblSesso = new JLabel("Sesso:");
+		lblSesso.setBounds(190, 29, 46, 14);
+		contentPane.add(lblSesso);
+		
+		JLabel lblEta = new JLabel("Eta:");
+		lblEta.setBounds(10, 104, 46, 14);
+		contentPane.add(lblEta);
+		
+		tesseraField = new JTextField();
+		tesseraField.setBounds(76, 29, 86, 20);
+		contentPane.add(tesseraField);
+		tesseraField.setColumns(10);
+		
+		nomeField = new JTextField();
+		nomeField.setBounds(76, 60, 86, 20);
+		contentPane.add(nomeField);
+		nomeField.setColumns(10);
+		
+		cognomeField = new JTextField();
+		cognomeField.setBounds(263, 60, 86, 20);
+		contentPane.add(cognomeField);
+		cognomeField.setColumns(10);
+		
+		sessoField = new JTextField();
+		sessoField.setBounds(263, 29, 86, 20);
+		contentPane.add(sessoField);
+		sessoField.setColumns(10);
+		
+		JButton submitButton = new JButton("Submit");
+		submitButton.setBounds(288, 104, 89, 23);
+		contentPane.add(submitButton);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"}));
+		comboBox.setBounds(76, 100, 46, 22);
+		contentPane.add(comboBox);
+
+		
+		
+		submitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String tessera = tesseraField.getText();
+				String nome = nomeField.getText();
+				String cognome = cognomeField.getText();
+				int eta = Integer.parseInt(comboBox.getSelectedItem().toString());
+				String sesso = sessoField.getText();
+				
+				
+				String sql = String.format("INSERT INTO Tennista values (\"%s\",   \"%s\",   \"%s\",   \"%s\",   %d);", tessera, nome, cognome, sesso, eta);
+				
+				try {
+					Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);	
+					PreparedStatement statement = connection.prepareStatement(sql);
+					statement.executeUpdate();
+										
+					System.out.println("Inserted value");
+					
+					statement.close();
+					connection.close();
+				} catch (SQLException ex) {
+					System.out.println("Cannot connect");
+					ex.printStackTrace();
+				}
+			}
+		});
+		
+		
+		this.getRootPane().setDefaultButton(submitButton);
+	}
+}
